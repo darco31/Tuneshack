af40c10c7a50031db0127b09a241dc462bf5e967
Stephen <sdarcy29@hotmail.com>
2022-04-15 10:21:37 +0000

    Add bootstrap form control class to forms and create forms.py file

--- musicblog/views.py
+++ musicblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DeleteView, CreateView
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -20,5 +21,6 @@ class PostView(DeleteView):
 class AddPost(CreateView):
 
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+    # fields = '__all__'
