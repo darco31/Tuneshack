c447f6f600974f8388a702eb7771cbfeb96fa41f
Stephen <sdarcy29@hotmail.com>
2022-04-17 18:56:12 +0000

    Add detail to README. Finish the url for the delte post and test the workings

--- musicblog/urls.py
+++ musicblog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, PostView, AddPost, EditPost
+from .views import HomeView, PostView, AddPost, EditPost, DeletePost
 
 urlpatterns = [
     # path('', views.home, name='home'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('posts/<int:pk>', PostView.as_view(), name='post-detail'),
     path('add_post/', AddPost.as_view(), name='add_post'),
     path('posts/edit_post/<int:pk>', EditPost.as_view(), name='edit_post'),
+    path('posts/<int:pk>/delete', DeletePost.as_view(), name='delete_post'),
 ]
--- musicblog/views.py
+++ musicblog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DeleteView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -10,6 +11,7 @@ class HomeView(ListView):
 
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 
 class PostView(DeleteView):
@@ -35,6 +37,12 @@ class EditPost(UpdateView):
 
 
 class DeletePost(DeleteView):
+    """
+    Use reverse lazy below to redirect user to
+    home page on deletion of post
+    https://docs.djangoproject.com/en/4.0/ref/urlresolvers/
+    """
 
     model = Post
     template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
