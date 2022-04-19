b63a641ca105651e2b2ed70c72b18bb6440652f3
Stephen <sdarcy29@hotmail.com>
2022-04-19 12:27:43 +0000

    Add new app for register members and add new urls file with paths and add new template folders

--- tuneshack/settings.py
+++ tuneshack/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'django.contrib.staticfiles',
     'musicblog',
+    'members',
 ]
 
 MIDDLEWARE = [
--- tuneshack/urls.py
+++ tuneshack/urls.py
@@ -5,5 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('musicblog.urls'))
+    path('', include('musicblog.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
