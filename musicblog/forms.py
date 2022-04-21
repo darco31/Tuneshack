0055027939300b0fff3147053351f46fd0d8aa8e
Stephen <sdarcy29@hotmail.com>
2022-04-21 15:43:55 +0000

    Add javascript to insert the current user that is logged in can add posts

--- musicblog/forms.py
+++ musicblog/forms.py
@@ -18,7 +18,8 @@ class PostForm(forms.ModelForm):
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Put your title here!!'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'username', 'type': 'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Express yourself'}),
         }
 
