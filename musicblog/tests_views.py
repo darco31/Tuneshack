c08270ecf09ed1ea5b745ef59d1f49a9b0227431
Stephen <sdarcy29@hotmail.com>
2022-04-26 18:50:03 +0000

    Change DB to SQLite for testing and set up files for views, models and forms

---
+++ musicblog/tests_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    # def test_get_post_page(self):
+
+    # def add_post_page(self):
+
+    # def delete_post_page(self):
+
+    # def add_comment_page(self):
+
+    # def edit_post_page(self):
