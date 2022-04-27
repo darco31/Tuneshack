d6c6b4fb45fbad739d52d792b972ba38e31d71db
Stephen <sdarcy29@hotmail.com>
2022-04-27 16:15:13 +0000

    Add tests to test files

--- musicblog/tests/tests_views.py
+++ musicblog/tests/tests_views.py
@@ -1,19 +1,27 @@
-# from django.test import TestCase
+# from django.test import TestCase, Client
+# from django.urls import reverse
+# from musicblog.models import Post, Comment
 
 
 # class TestViews(TestCase):
 
 #     def test_get_home_page(self):
-#         response = self.client.get('/')
-#         self.assertEqual(response.status_code, 200)
+
+#         client = Client()
+
+#         response = client.get(reverse('/'))
+#         self.assertEquals(response.status_code, 200)
 #         self.assertTemplateUsed(response, 'home.html')
 
-#     def test_get_post_page(self):
-#         response = self.client.get('add_comment')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'add_comment.html')
+    # def test_get_post_page(self):
+    #     response = self.client.get('/post-detail')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'post_detail.html')
 
     # def add_post_page(self):
+    #     response = self.client.get('/add_post')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'add_post.html')
 
     # def delete_post_page(self):
 
--- musicblog/urls.py
+++ musicblog/urls.py
@@ -6,7 +6,6 @@ from .views import (
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
     path('', HomeView.as_view(), name='home'),
     path('posts/<int:pk>', PostView.as_view(), name='post-detail'),
     path('add_post/', AddPost.as_view(), name='add_post'),
