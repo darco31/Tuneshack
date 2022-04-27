560392dacd79696006fd1af64016e7d1bc3cb2b6
Stephen <sdarcy29@hotmail.com>
2022-04-27 14:05:10 +0000

    Add form tests and favicon

--- musicblog/tests/test_forms.py
+++ musicblog/tests/test_forms.py
@@ -4,10 +4,14 @@ from musicblog.forms import PostForm, EditForm
 
 class TestPostForm(TestCase):
 
-    def test_item_name_required(self):
-        form = EditForm({'body': ''})
-        self.assertFalse(form.is_valid)
+    def test_title_field(self):
+        form = PostForm({'title': 'Test title'})
+        self.assertTrue(form.is_valid)
 
     def test_fields_explicit(self):
+        form = EditForm()
+        self.assertEqual(form.Meta.fields, ('title', 'body'))
+
+    def test_fields_post(self):
         form = PostForm()
         self.assertEqual(form.Meta.fields, ('title', 'author', 'body'))
