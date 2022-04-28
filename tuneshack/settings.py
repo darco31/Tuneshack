88a99f7635716412ebc925ff5d329bce28092580
Stephen <sdarcy29@hotmail.com>
2022-04-28 16:22:53 +0000

    Add messages to base html file

--- tuneshack/settings.py
+++ tuneshack/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 import dj_database_url
-
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -33,6 +33,14 @@ INSTALLED_APPS = [
     'musicblog',
     'members',
 ]
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
